e5295661c5e98b1144e4c9e2500e7a1a65c32e5b
LucasGuerraS <lucasgasardo@gmail.com>
2022-05-10 11:46:43 -0300

    fix no contador de login e senha

--- loginSenha.py
+++ loginSenha.py
@@ -1,17 +1,21 @@
 username = "LucasGuerraS"
 senha = "P@ssw0rd778"
-cont = 0
-while True:
-     x = input("Digite seu usuário: ")
-     y = input("Digite sua senha: ")
-     if x == username and y == senha:
-         print("Login efetuado com sucesso!")
-         break
-     else:
-        print("Usuário ou senha inválidos!")
-        cont += 1
-        if cont == 3:
-            print("Redefina o usuário e a senha por favor!")
-            username = input("Digite seu novo usuário: ")
-            senha = input("Digite sua nova senha: ")
-            
+continua = True
+
+while continua == True:
+    cont = 0
+    while cont <= 3:
+         x = input("Digite seu usuário: ")
+         y = input("Digite sua senha: ")
+         if x == username and y == senha:
+             print("Login efetuado com sucesso!")
+             continua = False
+             break
+         else:
+            print("Usuário ou senha inválidos!")
+            cont += 1
+            if cont == 3:
+                print("Redefina o usuário e a senha por favor!")
+                username = input("Digite seu novo usuário: ")
+                senha = input("Digite sua nova senha: ")
+                
