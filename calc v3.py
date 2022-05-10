1404dfe7bb450eb11a17a46542aa31819d33e19f
LucasGuerraS <lucasgasardo@gmail.com>
2022-05-10 11:02:34 -0300

    add var acumuladora e display de result

--- calc v3.py
+++ calc v3.py
@@ -3,11 +3,13 @@ print("\n (+) (-) (*) (/) (=)")
 
 n = int(input("Número: "))
 op = input("Operação: ")
+acum = n
 result = n
 maior = n
 menor = n
 while op != "=":
     n = int(input("Número: "))
+    acum += n
     if n > maior:
         maior = n
     if n < menor:
@@ -20,8 +22,10 @@ while op != "=":
         result *= n
     elif op == "/":
         result /= n
+    print(f"Resultado: {result}")
     op = input("Operação: ")
 print(f'Resultado: {result}')
 print(f'Maior: {maior}')
 print(f'Menor: {menor}')
+print(f"Total dos números digitados: {acum}")
 
